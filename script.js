8ed9857f5786cc629872d5eb8c492f984611c9ae
Gustavo Morales <Morales.fagundez@gmail.com>
2023-04-27 12:25:15 -0500

    Primer commit del Portfolio.

---
+++ script.js
@@ -0,0 +1,29 @@
+let menuVisible = false;
+//ocultar menu
+function  mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible =  true;
+    }
+}
+function seleccionar(){
+    //ocultar despues de seleccionar
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//animacion de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss")
+    }
+}
+window.onscroll = function(){
+    efectoHabilidades
+}
